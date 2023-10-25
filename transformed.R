21010305c7c71cbd995cf2058bc1c59aaace29ce
heySonia666 <M.Mo2@lse.ac.uk>
2023-10-25 17:03:26 +0100

    finish first transform

--- transformed.R
+++ transformed.R
@@ -1,3 +1,20 @@
-data("iris")
-head(iris)
-# define a function with a single argument data, that displays the iris dataset and performs its input transformation on it.
+# read and load the dataset "mtcars"
+data("mtcars")
+head(mtcars)
+
+transform_data <- function(data) {
+  # Check if the 'disp' column exists in the dataset
+  if (!'disp' %in% colnames(data)) {
+    stop("The dataset must contain an 'disp' column for Displacement.")
+  }
+  
+  # a function that applies a simple transformation: increasing the Displacement ('disp') of each car by 100%.
+  data$disp <- data$disp * 2
+  
+  # Return the transformed dataset
+  return(data)
+}
+
+# Example usage with the mtcars dataset
+transformed_data <- transform_data(mtcars)
+print(transformed_data)
