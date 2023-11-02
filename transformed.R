84c5c75c836983bad6bb055c046fa900577d83a1
heySonia666 <M.Mo2@lse.ac.uk>
2023-11-02 00:56:06 +0000

    delete the error version

--- transformed.R
+++ transformed.R
@@ -1,41 +1,37 @@
+library(dplyr)
 transform_data <- function(data, transformation_type = "increase_disp") {
   # Check if the necessary columns exist in the dataset
   required_columns <- c("disp", "mpg", "wt")
   if (!all(required_columns %in% colnames(data))) {
     stop("The dataset must contain the following columns: disp, mpg, wt.")
   }
-  
-  transformed_data <- data
-  
-  if (transformation_type == "increase_disp") {
-    # Increase the Displacement ('disp') of each car by 100%.
-    transformed_data$disp <- transformed_data$disp * 2
-    
-  } else if (transformation_type == "decrease_mpg") {
-    # Decrease the miles per gallon by 20%
-    transformed_data$mpg <- transformed_data$mpg * 0.8
-    
-  } else if (transformation_type == "increase_weight") {
-    # Increase the weight by 10%
-    transformed_data$wt <- transformed_data$wt * 1.1
-    
+# Apply different transformations based on the value of transformation_type
+  if (transformation_type == "triple") {
+    # Increase the Displacement ('disp') of each car by 200%
+    data <- data %>%
+      mutate(disp = disp * 3)
+  } else if (transformation_type == "double") {
+    # Increase the Displacement ('disp') of each car by 100%
+    data <- data %>%
+      mutate(disp = disp ^ 2)
+  } else if (transformation_type == "square") {
+     # Square the Displacement ('disp') of each car
+    data <- data %>%
+      mutate(disp = sqrt(disp))
   } else {
-    stop("Invalid transformation type. Please choose from 'increase_disp', 'decrease_mpg', or 'increase_weight'.")
+    stop("Invalid transformation type. Please choose 'triple', 'double', or 'square'.")
   }
   
   # Return the transformed dataset
-  return(transformed_data)
+  return(data)
 }
 
-# print the transformed mtcars dataset
-transformed_data_disp <- transform_data(mtcars, "increase_disp")
-print("Increased Horsepower:")
-print(transformed_data_disp)
+# Example usage with the mtcars dataset
+transformed_data_double <- transform_data(mtcars, "triple")
+print(transformed_data_triple)
 
-transformed_data_mpg <- transform_data(mtcars, "decrease_mpg")
-print("Decreased MPG:")
-print(transformed_data_mpg)
+transformed_data_square <- transform_data(mtcars, "double")
+print(transformed_data_double)
 
-transformed_data_wt <- transform_data(mtcars, "increase_weight")
-print("Increased Weight:")
-print(transformed_data_wt)
+transformed_data_root <- transform_data(mtcars, "square")
+print(transformed_data_square)
